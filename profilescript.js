d03190db6a081425d3733c92e7746b9e10b208e0
khushi_suryawanshi <khushisuryawanshi006@gmail.com>
2025-07-26 13:39:19 +0530

    Add files via upload

---
+++ profilescript.js
@@ -0,0 +1,8 @@
+document.getElementById("reveal-fact").addEventListener("click", function () {
+  const fact = document.getElementById("fact");
+  if (fact.style.display === "none") {
+    fact.style.display = "block";
+  } else {
+    fact.style.display = "none";
+  }
+});
